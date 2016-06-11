fdf18be191287d5ad1fed66c669a33b0f6f4051a
thao luu <thao luu>
2016-06-11 11:01:35 +0700

    first commit

---
+++ src/House.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Admin on 6/11/2016.
+ */
+public class House {
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@
  */
 public class Main {
     public static void main(String[] args){
-        GameWindow gameWindow = new GameWindow();
-        Thread thread = new Thread(gameWindow);
-        thread.start();
+
     }
 }
