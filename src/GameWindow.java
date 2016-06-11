04234c8254de15e05b5b9ed710c5f94931bc385d
Duy Chien <duychien122496@gmail.com>
2016-06-11 14:03:45 +0700

    Sua Player

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,7 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
@@ -12,12 +14,13 @@ import java.io.IOException;
 public class GameWindow extends Frame implements Runnable {
     Image background;
     BufferedImage bufferedImage;
+    Player player1;
 
-    public GameWindow() throws IOException {// constructor
-        this.setSize(750,750);
+    public GameWindow() {// constructor
+        this.setSize(750, 750);
         this.setTitle("Boom-Techkids");
         this.setVisible(true);
-        background = ImageIO.read(new File("Resources/background.jpg"));
+
         this.addWindowListener(new WindowListener() {
             @Override
             public void windowOpened(WindowEvent e) {
@@ -54,11 +57,51 @@ public class GameWindow extends Frame implements Runnable {
 
             }
         });// closing window
-    }
 
+        player1 = new Player(300,400);
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //vua an phim
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //phim duoc an va giu
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_W:
+                        player1.speedY = -3;
+                        break;
+                    case KeyEvent.VK_A:
+                        player1.speedX = -3;
+                        break;
+                    case KeyEvent.VK_S:
+                        player1.speedY = 3;
+                        break;
+                    case KeyEvent.VK_D:
+                        player1.speedX = 3;
+                        break;
+//                    case KeyEvent.VK_SPACE:
+//                        break;
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+                //tha phim ra
+                player1.speedX = 0;
+                player1.speedY = 0;
+            }
+        });
+        try {
+            background = ImageIO.read(new File("Resources/background.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void gameUpdate() {
         // do something here before repaint method reDraw components
-        System.out.println("Thay doi cac thuoc tinh cua doi tuong trươc ve lai");
+//        System.out.println("Thay doi cac thuoc tinh cua doi tuong trươc ve lai");
+        player1.update();
     }
 
     @Override
@@ -68,6 +111,7 @@ public class GameWindow extends Frame implements Runnable {
         }
         Graphics bufferedGraphics = bufferedImage.getGraphics();
         bufferedGraphics.drawImage(background, 0, 0, null);
+        player1.draw(bufferedGraphics);
         g.drawImage(bufferedImage, 0, 0,null);
 
     }
--- src/Player.java
+++ src/Player.java
@@ -3,22 +3,25 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by admin on 6/11/2016.
  */
-public class Player {
+public class Player implements IFighter {
     public int positionX;
     public int positionY;
     public int speedX;
     public int speedY;
     public BufferedImage image;
 
+    public ArrayList<Boom> listBooms;
+
     public Player(int positionX, int positionY)  {
         this.positionX = positionX;
         this.positionY = positionY;
         try {
-            this.image = ImageIO.read(new File("Resources/p474.jpg"));
+            this.image = ImageIO.read(new File("Resources/playerquayxuong.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,7 +30,7 @@ public class Player {
         this.positionX = 350;
         this.positionY = 300;
         try {
-            this.image = ImageIO.read(new File("Resources/p474.jpg"));
+            this.image = ImageIO.read(new File("Resources/playerquayxuong.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,8 +38,33 @@ public class Player {
     public void update(){
         this.positionX += this.speedX;
         this.positionY += this.speedY;
+        if (speedX>0) try {
+            this.image = ImageIO.read(new File("Resources/playerquayphai.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (speedX<0) try {
+            this.image = ImageIO.read(new File("Resources/playerquaytrai.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (speedY>0) try {
+            this.image = ImageIO.read(new File("Resources/playerquayxuong.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (speedY<0) try {
+            this.image = ImageIO.read(new File("Resources/playerquaylen.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public void draw(Graphics g){
         g.drawImage(this.image, this.positionX, this.positionY, null);
+    }
+
+    @Override
+    public void fire() {
+        listBooms.add(new Boom(positionX,positionY));
     }
 }
